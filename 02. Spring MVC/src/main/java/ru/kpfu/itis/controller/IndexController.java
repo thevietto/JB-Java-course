17510a8ed938260cac6f8ce98c5faad3b9adb943
thevietto <thevietto@gmail.com>
2015-10-08 19:57:19 +0300

    commit

--- 02. Spring MVC/src/main/java/ru/kpfu/itis/controller/IndexController.java
+++ 02. Spring MVC/src/main/java/ru/kpfu/itis/controller/IndexController.java
@@ -1,7 +1,10 @@
 package ru.kpfu.itis.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -12,4 +15,16 @@ public class IndexController {
         return "index";
     }
 
+    @RequestMapping("/test")
+    public String showAnotherPage(@RequestParam("result") String result, Model model) {
+        model.addAttribute("text", "Какой-то текст");
+        model.addAttribute("result", result);
+        return "another";
+    }
+
+    @RequestMapping("/test/{username}")
+    public String showUsername(@PathVariable("username") String username, Model model) {
+        model.addAttribute("username", username);
+        return "usernameshow";
+    }
 }
---
+++ 02. Spring MVC/src/main/webapp/resources/jsp/another.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: root
+  Date: 08.10.15
+  Time: 18:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Открылась новая страница</h1>
+
+${text}<br>
+${result}
+
+</body>
+</html>
