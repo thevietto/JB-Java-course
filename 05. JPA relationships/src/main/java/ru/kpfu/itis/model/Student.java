d2e181a18ea998cff9262228cc088363b4148cfa
thevietto <thevietto@gmail.com>
2015-10-13 23:17:45 +0300

    5th example ready

--- 05. JPA relationships/src/main/java/ru/kpfu/itis/model/Student.java
+++ 05. JPA relationships/src/main/java/ru/kpfu/itis/model/Student.java
@@ -1,5 +1,16 @@
 package ru.kpfu.itis.model;
 
+import javax.persistence.Entity;
 
+@Entity
 public class Student {
+
+    private String name;
+
+    private String surname;
+
+    private String middlename;
+
+    public Student() {
+    }
 }
