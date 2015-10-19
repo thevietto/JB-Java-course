03ea5bb3b736aad00273787b2864fd388421a7bc
thevietto <thevietto@gmail.com>
2015-10-19 14:06:48 +0300

    6th example ready

--- 06. Transactions/src/main/java/ru/kpfu/itis/repository/StudentRepository.java
+++ 06. Transactions/src/main/java/ru/kpfu/itis/repository/StudentRepository.java
@@ -1,8 +1,9 @@
 package ru.kpfu.itis.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.kpfu.itis.model.Student;
 
-
+@Repository
 public interface StudentRepository {
 
     Student find(Long id);
--- 06. Transactions/src/main/java/ru/kpfu/itis/service/StudentService.java
+++ 06. Transactions/src/main/java/ru/kpfu/itis/service/StudentService.java
@@ -1,8 +1,10 @@
 package ru.kpfu.itis.service;
 
 
+import org.springframework.stereotype.Service;
 import ru.kpfu.itis.model.Student;
 
+@Service
 public interface StudentService {
 
     void saveStudent(Student student);
