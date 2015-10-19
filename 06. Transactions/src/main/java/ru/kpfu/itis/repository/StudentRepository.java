49576db1bac3ae90e33f96ded718018fe43face6
thevietto <thevietto@gmail.com>
2015-10-19 14:58:06 +0300

    6th example ready

--- 06. Transactions/src/main/java/ru/kpfu/itis/repository/StudentRepository.java
+++ 06. Transactions/src/main/java/ru/kpfu/itis/repository/StudentRepository.java
@@ -1,13 +1,16 @@
 package ru.kpfu.itis.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.kpfu.itis.model.Student;
 
+import java.util.List;
+
 @Repository
-public interface StudentRepository {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    Student find(Long id);
-
-    void save(Student student);
+//    @Query("SELECT s FROM Student AS s")
+    List<Student> findByAdressStreet(String street);
 
 }
