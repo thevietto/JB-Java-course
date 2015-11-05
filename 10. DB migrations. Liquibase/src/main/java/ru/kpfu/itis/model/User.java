58dd6738b9ec73094d0cdca30d99b09f985f3a5b
thevietto <thevietto@gmail.com>
2015-11-05 18:57:24 +0300

    10th example ready

--- 10. DB migrations. Liquibase/src/main/java/ru/kpfu/itis/model/User.java
+++ 10. DB migrations. Liquibase/src/main/java/ru/kpfu/itis/model/User.java
@@ -5,11 +5,12 @@ import ru.kpfu.itis.model.enums.UserRole;
 import javax.persistence.*;
 
 @Entity
+@SequenceGenerator(name = "user_gen", sequenceName = "users_seq", allocationSize = 1)
 @Table(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_gen")
     private long id;
 
     private String username;
